92c304e99a448e826f346ba311157861e9f3b93b
Abdul Haseeb <haseeb_vic@hotmail.com>
2015-01-05 16:43:05 +0500

    IMS Solution (1/5/2015)
    Commited by Abdul Haseeb, the initial structure of Inventory Management
    System

---
+++ IMS_v1/IMS_v1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IMS_v1.Startup))]
+namespace IMS_v1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
